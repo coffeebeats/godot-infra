b9cb5dd36d57cd4cbb667baa664aee093884dddb
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2024-03-12 21:49:07 -0700

    chore(containers): activate python3 environment

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -5,6 +5,10 @@ set -Eeuo pipefail
 # This script is an entrypoint for the 'godot-infra' image.
 #
 
+# ------------------------- Configure: Python3 (venv) ------------------------ #
+
+. .venv/bin/activate
+
 # -------------------------- Run: Passed-in command -------------------------- #
 
 exec "$@"
