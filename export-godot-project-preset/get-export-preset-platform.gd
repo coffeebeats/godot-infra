27c5ea0eda6e02c3c53352168cf627656c40dc63
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2024-04-30 21:54:53 -0700

    refactor(export): simplify godot scripts for manipulating `export_presets.cfg`

--- export-godot-project-preset/get-export-preset-platform.gd
+++ export-godot-project-preset/get-export-preset-platform.gd
@@ -1,28 +1,25 @@
 extends SceneTree
 
 
+func fail(msg: String):
+	push_error(msg)
+	quit(1)
+
+
 func _initialize():
+	var path_export_presets := OS.get_environment("EXPORT_PRESETS_PATH")
+	if not path_export_presets:
+		return fail("missing environment variable: 'EXPORT_PRESETS_PATH'")
+
+	var preset := OS.get_environment("PRESET")
+	if not preset:
+		return fail("missing environment variable: 'PRESET'")
+
 	var config := ConfigFile.new()
 
-	var cfg := {}
-
-	for arg in OS.get_cmdline_args().slice(2):
-		var parts := arg.lstrip("-").split("=")
-		_assert(len(parts) == 2, "invalid argument received: " + arg)
-
-		cfg[parts[0]] = parts[1]
-
-	var path_export_presets: String = cfg.get("export-presets-path", "")
-	if _assert(path_export_presets != "", "missing required flag: 'export-presets-path'"):
-		return
-
-	var preset: String = cfg.get("preset", "")
-	if _assert(preset != "", "missing required flag: 'preset'"):
-		return
-
 	var err := config.load("res://" + path_export_presets)
-	if _assert(err == OK, "failed to load file: " + path_export_presets):
-		return
+	if err != OK:
+		return fail("failed to load file: " + path_export_presets)
 
 	for section in config.get_sections():
 		if section.ends_with("options"):
@@ -32,8 +29,8 @@ func _initialize():
 			continue
 
 		var platform: String = config.get_value(section, "platform")
-		if _assert(platform != "", "missing required property: " + section + "/platform"):
-			return
+		if platform == "":
+			return fail("missing required property: " + section + "/platform")
 
 		match platform:
 			"Linux/X11":
@@ -46,15 +43,4 @@ func _initialize():
 				print("windows")
 				return
 
-	_assert(false, "failed to find preset: " + preset)
-
-
-func _assert(expr: bool, message: String) -> int:
-	if expr:
-		return OK
-
-	if message:
-		push_error(message)
-
-	quit(1)
-	return FAILED
+	fail("failed to find preset: " + preset)
