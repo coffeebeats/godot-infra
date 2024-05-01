15461c3525f00654072ce27a2546d1fe52b3a042
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2024-04-30 21:55:07 -0700

    refactor(export): simplify godot scripts for manipulating `export_presets.cfg` (#139)

--- export-godot-project-preset/set-export-template-path.gd
+++ export-godot-project-preset/set-export-template-path.gd
@@ -1,32 +1,29 @@
 extends SceneTree
 
 
+func fail(msg: String):
+	push_error(msg)
+	quit(1)
+
+
 func _initialize():
 	var config := ConfigFile.new()
 
-	var cfg := {}
+	var path_export_presets := OS.get_environment("EXPORT_PRESETS_PATH")
+	if not path_export_presets:
+		return fail("missing environment variable: 'EXPORT_PRESETS_PATH'")
 
-	for arg in OS.get_cmdline_args().slice(2):
-		var parts := arg.lstrip("-").split("=")
-		_assert(len(parts) == 2, "invalid argument received: " + arg)
+	var preset := OS.get_environment("PRESET")
+	if not preset:
+		return fail("missing environment variable: 'PRESET'")
 
-		cfg[parts[0]] = parts[1]
-
-	var path_export_presets: String = cfg.get("export-presets-path", "")
-	if _assert(path_export_presets != "", "missing required flag: 'export-presets-path'"):
-		return
-
-	var preset: String = cfg.get("preset", "")
-	if _assert(preset != "", "missing required flag: 'preset'"):
-		return
-
-	var template_path: String = cfg.get("template-path", "")
-	if _assert(template_path != "", "missing required flag: 'template-path'"):
-		return
+	var path_template := OS.get_environment("TEMPLATE_PATH")
+	if not path_template:
+		return fail("missing environment variable: 'TEMPLATE_PATH'")
 
 	var err := config.load("res://" + path_export_presets)
-	if _assert(err == OK, "failed to load file: " + path_export_presets):
-		return
+	if err != OK:
+		return fail("failed to load file: " + path_export_presets)
 
 	for section in config.get_sections():
 		if section.ends_with("options"):
@@ -35,22 +32,11 @@ func _initialize():
 		if config.get_value(section, "name") != preset:
 			continue
 
-		config.set_value(section + ".options", "custom_template/release", template_path)
-		config.set_value(section + ".options", "custom_template/debug", template_path)
+		config.set_value(section + ".options", "custom_template/release", path_template)
+		config.set_value(section + ".options", "custom_template/debug", path_template)
 
 		err = config.save("res://" + path_export_presets)
-		if _assert(err == OK, "failed to save file: " + path_export_presets):
-			return
+		if err != OK:
+			return fail("failed to save file: " + path_export_presets)
 
 		break
-
-
-func _assert(expr: bool, message: String) -> int:
-	if expr:
-		return OK
-
-	if message:
-		push_error(message)
-
-	quit(1)
-	return FAILED
