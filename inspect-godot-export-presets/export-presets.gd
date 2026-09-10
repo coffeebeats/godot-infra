ebbbe27b9bde4bb351160d0685125a06f75f6b37
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2026-09-10 12:09:17 -0700

    fix(export): support strict GDScript warnings

--- inspect-godot-export-presets/export-presets.gd
+++ inspect-godot-export-presets/export-presets.gd
@@ -1,12 +1,12 @@
 extends SceneTree
 
 
-func fail(msg: String):
+func _fail(msg: String) -> void:
 	push_error(msg)
 	quit(1)
 
 
-func _initialize():
+func _initialize() -> void:
 	var path_export_presets := OS.get_environment("EXPORT_PRESETS_PATH")
 	if not path_export_presets:
 		path_export_presets = "export_presets.cfg"
@@ -15,34 +15,42 @@ func _initialize():
 
 	var err := cfg.load("res://" + path_export_presets)
 	if err != OK:
-		return fail("failed to load file: " + path_export_presets)
+		_fail("failed to load file: " + path_export_presets)
+		return
 
 	var args := OS.get_cmdline_user_args()
 
 	if not args:
-		return fail("missing required arguments: <OPERATION> <PRESET> <KEY> [VALUE]")
+		_fail("missing required arguments: <OPERATION> <PRESET> <KEY> [VALUE]")
+		return
 
 	var op := args[0]
 	if op != "get" and op != "set":
-		return fail("unexpected operation; wanted 'get' or 'set', but was: %s" % args[0])
+		_fail("unexpected operation; wanted 'get' or 'set', but was: %s" % args[0])
+		return
 
 	if args[0] == "get" and len(args) != 3:
-		return fail("unexpected input; wanted <PRESET> <KEY>, but was: %s" % args.slice(1))
+		_fail("unexpected input; wanted <PRESET> <KEY>, but was: %s" % args.slice(1))
+		return
 
 	if args[0] == "set" and len(args) != 4:
-		return fail("unexpected input; wanted <PRESET> <KEY> <VALUE>, but was: %s" % args.slice(1))
+		_fail("unexpected input; wanted <PRESET> <KEY> <VALUE>, but was: %s" % args.slice(1))
+		return
 
 	var preset := args[1]
 	if not preset:
-		return fail("missing argument: 'preset'")
+		_fail("missing argument: 'preset'")
+		return
 
 	var key := args[2]
 	if not key:
-		return fail("missing argument: 'key'")
+		_fail("missing argument: 'key'")
+		return
 
 	var index := _find_preset_index(cfg, preset)
 	if index == -1:
-		return fail("failed to find preset: " + preset)
+		_fail("failed to find preset: " + preset)
+		return
 
 	var section := "preset." + str(index)
 	if key.begins_with("options."):
@@ -61,7 +69,8 @@ func _initialize():
 			cfg.set_value(section, key, value)
 
 			if cfg.save("res://" + path_export_presets) != OK:
-				return fail("failed to save file: " + path_export_presets)
+				_fail("failed to save file: " + path_export_presets)
+				return
 
 
 func _find_preset_index(cfg: ConfigFile, preset: String) -> int:
